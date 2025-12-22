bbfe6fab3ced5484c9b0e1deb9c9b5e07030d34b
cipdv <cip.devries@gmail.com>
2025-12-21 23:02:13 -0500

    deploying

--- app/app/dashboard/records/page.tsx
+++ app/app/dashboard/records/page.tsx
@@ -1,8 +1,13 @@
 export default function RecordsDashboardPage() {
   return (
     <div className="pb-12 space-y-4 mt-8">
-      <h1 className="text-2xl font-semibold text-neutral-900">Manage records</h1>
-      <p className="text-base text-neutral-800">Manage records</p>
+      <h1 className="text-2xl font-semibold text-neutral-900">
+        Manage records
+      </h1>
+      <p className="text-base text-neutral-800">
+        Manage records. -update original status (if changing to sold, to who/how
+        much/when) -update original location (where is it rn) -
+      </p>
     </div>
   );
 }
